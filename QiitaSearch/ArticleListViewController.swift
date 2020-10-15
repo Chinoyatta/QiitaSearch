cd76ecb9bf097a78cda7f8dd6e79b44d24d3ac40
Eiichi Chino <chinopro0717@gmail.com>
2020-10-15 22:41:54 +0900

    add:ArticleListViewController

---
+++ QiitaSearch/ArticleListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ArticleListViewController.swift
+//  QiitaSearch
+//
+//  Created by 千野栄一 on 2020/10/15.
+//
+
+import UIKit
+
+class ArticleListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
