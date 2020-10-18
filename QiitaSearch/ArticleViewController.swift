263884891d97ff8e442c0fb1fdb704d4c0504941
Eiichi Chino <chinopro0717@gmail.com>
2020-10-18 16:44:30 +0900

    add:WKWebView表示処理の追加

--- QiitaSearch/ArticleViewController.swift
+++ QiitaSearch/ArticleViewController.swift
@@ -6,24 +6,39 @@
 //
 
 import UIKit
+import WebKit
 
 class ArticleViewController: UIViewController {
-
+    
+    let webView = WKWebView()
+    let articleListVC = ArticleListViewController()
+    var url: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        webView.frame = self.view.frame
+        self.view.addSubview(webView)
+        
+        loadURL()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func loadURL() {
+        guard let myUrl = URL(string: url!) else {
+            return
+        }
+        let req = URLRequest(url: myUrl)
+        webView.load(req as URLRequest)
     }
-    */
-
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
